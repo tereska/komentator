5b205e480fe96eccd074af883708f34407ec807e
root <root@vm-10-177-48-186.c1r1.onet>
2012-05-08 22:03:24 +0200

    screen scripts

--- mc.sh
+++ mc.sh
@@ -1,3 +1,5 @@
 
 screen -S $NAME -X quit
 screen -dmS $NAME -c ./mc.screenrc
+
+#NAME=S7 PORT=8007 IP=10.177.48.186 ./mc.sh
---
+++ sv.sh
@@ -0,0 +1,3 @@
+
+screen -S C1M -X quit
+screen -dmS C1M -c ./sv.screenrc
